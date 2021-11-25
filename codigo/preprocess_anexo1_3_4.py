eaf62921b62c9e7070eeddb8c2d585cab433ec56
Augusto D. Camargo <augusto.duraes33@gmail.com>
2021-11-24 23:24:01 -0300

    adição datas e páginas

--- codigo/preprocess_anexo1_3_4.py
+++ codigo/preprocess_anexo1_3_4.py
@@ -16,9 +16,16 @@ import warnings
 #warnings.filterwarnings("ignore", 'This pattern has match groups')
 
 # Para rodar no linux, é necessário tirar a barra do começo do path por lagum motivo
+
+# Pasta com os arquivos que serão processados
 path_entrada = "dados/1_dados_entrada"
+# Pasta com os arquivos após o processamento
+path_tratados = "dados/2_dados_tratados"
 exemplo = os.path.join(path_entrada, "PLOA2015_anex.pdf")
 
+# Arquivos de cada ano, e as respectivas páginas em cada arquivo
+dic_ano_pag = {2015: [1, 4, 5], 2016: [1, 4, 5], 2017: [1, 3, 4],
+               2018: [1, 3, 4], 2019: [1, 3, 4], 2020: [], 2021: [], 2022: []}
 
 # Selecionar as páginas referentes aos anexos 1,3 e 4
 text = extract_text(exemplo, page_numbers=[4])
