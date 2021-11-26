bf3352055749c8c3bca70fbc2ba3433a0b74bf7f
Augusto D. Camargo <augusto.duraes33@gmail.com>
2021-11-26 01:17:24 -0300

    inicio EDA dos CSVs do governo

--- codigo/preprocess_anexo1_3_4.py
+++ codigo/preprocess_anexo1_3_4.py
@@ -25,7 +25,12 @@ exemplo = os.path.join(path_entrada, "PLOA2015_anex.pdf")
 
 # Arquivos de cada ano, e as respectivas páginas em cada arquivo
 dic_ano_pag = {2015: [1, 4, 5], 2016: [1, 4, 5], 2017: [1, 3, 4],
-               2018: [1, 3, 4], 2019: [1, 3, 4], 2020: [], 2021: [], 2022: []}
+               2018: [1, 3, 4], 2019: [1, 3, 4], 2020: [1, 3, 4],
+               2021: [1, 3, 4], 2022: [1, 3, 4]}
+
+for ano in dic_ano_pag.values():
+    print(ano)
+    arquivo = os.path.join(path_entrada, "PLOA" + str(ano) + "_anex.pdf")
 
 # Selecionar as páginas referentes aos anexos 1,3 e 4
 text = extract_text(exemplo, page_numbers=[4])
