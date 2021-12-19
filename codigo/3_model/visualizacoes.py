b16c408c5de85f1350a30efe3a6b37023e1b7397
GabrielPimenta1999 <gabriel.pimenta1999@gmail.com>
2021-12-19 00:17:16 -0300

    Atualização das visualizações dos dados

--- codigo/3_model/visualizacoes.py
+++ codigo/3_model/visualizacoes.py
@@ -9,36 +9,8 @@ from typing import Callable, Any, Iterable
 sys.path.append('..\\..')
 DATA_PATH_TRATADOS = "../../dados/2_dados_tratados"
 
-def historico_orcamento(orcamento:pd.DataFrame) -> go.Figure:
-    '''
-    Calcula o histórico do orçamento inicial, atualizado, empenhado e realizado
 
-    Parâmetros:
-    -----------
-        1. orcamento {pd.DataFrame}: 
-            arquivo com o histórico orçamentário.
-
-    Saídas:
-    -------
-        1. fig {plotly.graph_objects.Figure}:
-            figura com o gráfico do histórico do orçamento
-    '''
-    aux = orcamento.groupby('exercicios').sum().reset_index()
-    aux.iloc[:, 1:] = aux.iloc[:, 1:]/1e9
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(name='Orçamento inicial', x=aux.exercicios, y=aux.orcamento_inicial, mode='lines'))
-    fig.add_trace(go.Scatter(name='Orçamento atualizado', x=aux.exercicios, y=aux.orcamento_atualizado, mode='lines'))
-    fig.add_trace(go.Scatter(name='Orçamento empenhado', x=aux.exercicios, y=aux.orcamento_empenhado, mode='lines'))
-    fig.add_trace(go.Scatter(name='Orçamento realizado', x=aux.exercicios, y=aux.orcamento_realizado, mode='lines'))
-
-
-    fig.update_layout(title='Orçamento brasileiro de 2014 a 2021',
-                      yaxis_title='Orçamento (em R$ bilhões)')
-
-    return fig
-
-def historico_orcamento_filter(orcamento:pd.DataFrame, filtros:dict) -> go.Figure:
+def historico_orcamento(orcamento:pd.DataFrame, *args:dict) -> go.Figure:
     '''
     Calcula o histórico do orçamento filtrado para algum
 
@@ -46,40 +18,95 @@ def historico_orcamento_filter(orcamento:pd.DataFrame, filtros:dict) -> go.Figur
     -----------
         1. orcamento {pd.DataFrame}: 
             arquivo com o histórico orçamentário;
-        2. filtros {dict}:
-            dicionário com as keys e os valores a serem filtrados.
+        2. args {dict}:
+            dicionário com os filtros que serão aplicados no dataset.
   
     Saídas:
     -------
         1. fig {plotly.graph_objects.Figure}:
             figura com o gráfico do histórico do orçamento
     '''
-    chaves = list(filtros.keys())
-    valores = list(filtros.values())
 
     aux = orcamento.copy()
-    for i, chave in enumerate(chaves):
-        aux = aux[aux[chave].isin(valores[i])]
+    valor = ['brasileiro']
+    for dicio in args:
+        chave = list(dicio.keys())[0]
+        valor = list(dicio.values())[0]
+        aux = aux[aux[chave].isin(valor)]
+    valor = '|'.join(valor)
+
     aux = aux.groupby('exercicios').sum().reset_index()
-    aux.iloc[:, 1:] = aux.iloc[:, 1:]/1e9
 
     fig = go.Figure()
-    fig.add_trace(go.Scatter(name='Orçamento inicial', x=aux.exercicios, y=aux.orcamento_inicial, mode='lines'))
-    fig.add_trace(go.Scatter(name='Orçamento atualizado', x=aux.exercicios, y=aux.orcamento_atualizado, mode='lines'))
-    fig.add_trace(go.Scatter(name='Orçamento empenhado', x=aux.exercicios, y=aux.orcamento_empenhado, mode='lines'))
-    fig.add_trace(go.Scatter(name='Orçamento realizado', x=aux.exercicios, y=aux.orcamento_realizado, mode='lines'))
+    fig.add_trace(go.Scatter(name='Orçamento inicial', x=aux.exercicios, y=aux.orcamento_inicial, mode='lines+markers'))
+    fig.add_trace(go.Scatter(name='Orçamento atualizado', x=aux.exercicios, y=aux.orcamento_atualizado, mode='lines+markers'))
+    fig.add_trace(go.Scatter(name='Orçamento empenhado', x=aux.exercicios, y=aux.orcamento_empenhado, mode='lines+markers'))
+    fig.add_trace(go.Scatter(name='Orçamento realizado', x=aux.exercicios, y=aux.orcamento_realizado, mode='lines+markers'))
 
 
-    fig.update_layout(title='Orçamento brasileiro de 2014 a 2021',
-                      yaxis_title='Orçamento (em R$ bilhões)')
+    fig.update_layout(title='Orçamento: ' + valor,
+                      yaxis_title='Orçamento (em R$)')
 
     return fig
+
+def composicao_filtro(orcamento:pd.DataFrame, 
+                      subdivisao:str, 
+                      intervalo:list = [np.datetime64('2014'), np.datetime64('2021')], 
+                      *args:dict) -> go.Figure:
+    '''
+    Calcula a composição média do orçamento por subdivisão
+
+    Parâmetros:
+    -----------
+        1. orcamento {pd.DataFrame}: 
+            arquivo com o histórico orçamentário;
+        2. subdivisao {str}:
+            variável pelo qual o orçamento será plotado
+        3. intervalo {list}:
+            intervalo de tempo analisado com as datas em questão
+        4. args {dict}:
+            dicionário com os filtros que serão aplicados no dataset.
+
+  
+    Saídas:
+    -------
+        1. fig {plotly.graph_objects.Figure}:
+            figura com o gráfico da composição do orçamento
+    '''
+
+    aux = orcamento.copy()
+    valor = ['brasileiro']
+    for dicio in args:
+        chave = list(dicio.keys())[0]
+        valor = list(dicio.values())[0]
+        aux = aux[aux[chave].isin(valor)]      
+    valor = '|'.join(valor)
+    aux = aux[(aux.exercicios >= intervalo[0])&(aux.exercicios <= intervalo[1])]
+    aux = aux.groupby([subdivisao, 'exercicios']).sum().reset_index()
+
+    fig = go.Figure()
+    data_pivo = intervalo[0]
+    while data_pivo <= intervalo[1]:
+        aux2 = aux[aux.exercicios.dt.year == int(np.datetime_as_string(data_pivo, unit='Y'))]
+        fig.add_trace(go.Bar(x=aux2[subdivisao], y=aux2.orcamento_realizado, name=np.datetime_as_string(data_pivo, unit='Y')))
+        data_pivo += np.timedelta64(1,'Y')
+
+
+    fig.update_yaxes(type="log")
+    fig.update_layout(title='Orçamento: ' + valor,
+                      yaxis_title='Orçamento (em R$)',
+                      xaxis={'categoryorder':'total descending'})
+
+    return fig
+
 
 if __name__ == '__main__':
     orcamento = pd.read_pickle(os.path.join(DATA_PATH_TRATADOS, 'orcamento_tratado.pkl'))
     PIB = pd.read_pickle(os.path.join(DATA_PATH_TRATADOS, 'PIB.pkl'))
 
-    fig1 = historico_orcamento(orcamento)
-    fig2 = historico_orcamento_filter(orcamento, {'nome_orgao_sup':['Ministério da Educação']})
+    fig1 = historico_orcamento(orcamento, {'nome_orgao_sup':['Ministério da Educação']}, {'nome_orgao_sub':['Fundação Universidade Federal de Uberlândia']})
+    fig2 = composicao_filtro(orcamento, 'nome_funcao', [np.datetime64('2014'), np.datetime64('2021')], {'nome_orgao_sup':['Ministério da Educação']})
+
     fig1.show()
     fig2.show()
+
